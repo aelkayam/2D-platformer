0aa23b0e897a82be3d692702511d25e45b685aee
Elkayam Melesse <elka@duck.com>
2022-09-22 11:27:42 +0300

    Delete Actor.gd
    deleted duplicate file

--- src/Actors/Actor.gd
+++
@@ -1,9 +0,0 @@
-extends KinematicBody2D
-class_name Actor
-
-const FLOOR_NORMAL = Vector2.UP
-
-export var gravity := 2000.0
-export var max_speed := Vector2(300.0, 1000.0)
-
-var _velocity := Vector2.ZERO
